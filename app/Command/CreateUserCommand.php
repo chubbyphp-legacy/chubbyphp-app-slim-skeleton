1ef6fe8ded9b0b68d0974ed8b2fcfe46ddae37e3
Dominik Zogg <dominik.zogg@gmail.com>
2016-10-30 23:10:24 +0100

    add final

--- app/Command/CreateUserCommand.php
+++ app/Command/CreateUserCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CreateUserCommand extends Command
+final class CreateUserCommand extends Command
 {
     /**
      * @var PasswordManagerInterface
